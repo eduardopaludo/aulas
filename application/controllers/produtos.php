f0b3f59cf385d3a6393017e538bd36305a76664e
Eduardo Paludo <eduardopaludo@icloud.com>
2015-04-24 00:23:24 -0300

    Adicionado helper de formulario

--- application/controllers/produtos.php
+++ application/controllers/produtos.php
@@ -1,13 +1,16 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Produtos extends CI_Controller {
 	public function index(){
-		$produtos = array();
-		$bola = array("nome" => "Bola de futebol", "descricao" => "usada na copa do mundo", "preco" => 300);
-		$raquete = array("nome" => "Butterfly", "descricao" => "usada pelo melhor mesatenista", "preco" => 500);
-		array_push($produtos, $bola, $raquete);
+		
+		$this->load->database();
+		$this->load->model("produtos_model");
+		$produtos = $this->produtos_model->buscaTodos();
 
 		$dados = array("produtos" => $produtos);
+		$this->load->helper("url");
+		$this->load->helper("currency");
+		$this->load->helper("form");
 		$this->load->view("produtos/index.php", $dados);
 	}
 }
