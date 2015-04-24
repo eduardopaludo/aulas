57d2a7e955edbb64ba4a12a93da018d96c0975d1
Eduardo Paludo <eduardopaludo@icloud.com>
2015-04-24 00:25:50 -0300

    Criado formulario de cadastro de usuarios

--- application/views/produtos/index.php
+++ application/views/produtos/index.php
@@ -1,6 +1,6 @@
 <html lang="en">
 	<head>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="<?= base_url("css/bootstrap.css")?>">
 	</head>
 	<body>
 		<div class="container">
@@ -10,10 +10,44 @@
 				<?php foreach ($produtos as $produto) : ?>
 				<tr>
 					<td><?=$produto["nome"]?></td>
-					<td><?=$produto["preco"]?></td>
+					<td><?=numeroEmReais($produto["preco"])?></td>
 				</tr>
 				<?php endforeach?>
 			</table>
+			
+
+			<h1>Cadastro de Usuario</h1>
+			<?php
+				echo form_open("usuarios/novo");
+
+				echo form_label("Nome", "nome");
+				echo form_input(array(
+					"name"      => "nome",
+					"id"        => "nome",
+					"class"     => "form-control",
+					"maxlength" => "255"
+				));
+				echo form_label("Email", "email");
+				echo form_input(array(
+					"name"      => "email",
+					"id"        => "email",
+					"class"     => "form-control",
+					"maxlength" => "255"
+				));
+				echo form_label("Senha", "senha");
+				echo form_password(array(
+					"name"      => "senha",
+					"id"		=> "senha",
+					"class"		=> "form-control",
+					"maxlength" => "255"
+				));
+				echo form_button(array(
+					"class"		=> "btn btn-primary",
+					"content"	=> "Cadastrar",
+					"type"		=> "submit"
+				));
+				echo form_close();
+			?>
 		</div>
 	</body>
 </html>
